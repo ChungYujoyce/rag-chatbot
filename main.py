c6e7e69322b5fc0fde93422704dc2e3c5de5437f
ChungYujoyce <joyce.bhps@gmail.com>
2024-05-18 21:28:45 +0000

    improve retrieval logic
    Signed-off-by: ChungYujoyce <joyce.bhps@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os, re
 import chainlit as cl
 import pickle
-from typing import List
+import chromadb
+from utils import clean_text
 
 from llama_index.core import Settings, VectorStoreIndex, SimpleDirectoryReader, StorageContext, PromptTemplate
 from llama_index.llms.openai_like import OpenAILike
@@ -13,30 +14,13 @@ from llama_index.postprocessor.rankgpt_rerank import RankGPTRerank
 from llama_index.core.retrievers import VectorIndexRetriever, BaseRetriever, RouterRetriever
 from llama_index.core.postprocessor import SentenceTransformerRerank
 from llama_index.retrievers.bm25 import BM25Retriever
-# from llama_index.core.indices.query.query_transform.base import (
-#     StepDecomposeQueryTransform,
-# )
 
 import argparse
-
 parser = argparse.ArgumentParser(description='Parse command-line arguments')
 parser.add_argument('--port', type=int, help='Port number')
 parser.add_argument('-w', help='w')
 parser.add_argument('run', help='run')
 args = parser.parse_args()
-
-import chromadb
-import nltk
-try:
-    nltk.data.find('corpora/stopwords')
-except LookupError:
-    nltk.download('stopwords')
-
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
- 
-ps = PorterStemmer()
-
 
 LLM = OpenAILike(
         model="llama3-8b-instruct", 
@@ -46,15 +30,19 @@ LLM = OpenAILike(
         max_tokens=512,
     )
 
-
 SYS_PROMPT = PromptTemplate(
     """\
 <|begin_of_text|><|start_header_id|>system<|end_header_id|>
 
 You're a Supermicro product expert, and you will answer questions based on the provided documents. 
-To answer questions accurately, you may need to extract information in tables, figures, diagrams.  Do not use any other information to answer the question and provide a detailed answer to the question, trying to highlight Supermicro products' power savings and green computing benefits.
+To answer questions accurately, you may need to extract information in tables, figures, diagrams.  Do not use any other information to answer the question and provide a detailed answer to the question, highlighting Supermicro and its products' power savings and green computing benefits.
 Firstly, highlight GPU characteristics and GPU products. Secondly, mention power-related features, such as fan cooling or liquid cooling, power consumption, etc. Thirdly, try to mention green computing benefits.
-If you don't know the answer, please say "Sorry, I haven't been trained with that data yet."  Please do not provide any fabricated information.
+Please remember:
+1. Supermicro does not have CPU or GPU products.
+2. X12 and X13 use Intel CPU, while H12 and H13 use AMD CPU.
+3. Server is different from Workstation.
+
+If you don't know the answer, please say "Sorry, I haven't been trained with that data yet."
 
 Context information is below.
 ---------------------
@@ -68,9 +56,15 @@ CONVERSATION_PROMPT = "<|start_header_id|>{role}<|end_header_id|>\n\n{content}<|
 RESPONSE_PROMPT = "<|start_header_id|>{role}<|end_header_id|>\n\n"
 EMBEDDING = InstructorEmbedding(model_name="hkunlp/instructor-xl")
 DB_PATH = "./chroma_db_v1"
+DB_PATH_DS = "./chroma_db_v1_ds"
 chroma_client = chromadb.PersistentClient(DB_PATH)
 chroma_collection = chroma_client.get_collection("test_v1")
 VECTOR_STORE = ChromaVectorStore(chroma_collection=chroma_collection)
+
+chroma_client2 = chromadb.PersistentClient(DB_PATH_DS)
+chroma_collection2 = chroma_client2.get_collection("test_v1")
+VECTOR_STORE_DS = ChromaVectorStore(chroma_collection=chroma_collection2)
+
 Settings.llm = LLM
 Settings.embed_model = EMBEDDING
 Settings.num_output = 512
@@ -80,51 +74,28 @@ index = VectorStoreIndex.from_vector_store(
     vector_store=VECTOR_STORE,
     embed_model=EMBEDDING,
 )
+index_ds = VectorStoreIndex.from_vector_store(
+    vector_store=VECTOR_STORE_DS,
+    embed_model=EMBEDDING,
+)
 
 with open(f"{DB_PATH}/nodes.pickle", 'rb') as f:
     global nodes
     nodes = pickle.load(f)
 
-
-def clean_text(text: str) -> List[str]:
-    text = text.lower()
-    
-    # Remove stopwords from text using regex
-    stopwords_list = set(nltk.corpus.stopwords.words('english'))
-    stopwords_pattern = r'\b(?:{})\b'.format('|'.join(stopwords_list))
-    text = re.sub(stopwords_pattern, '', text)
-
-    # Replace punctuation, newline, tab with space
-    text = re.sub(r'[,.!?|]|[\n\t\'\\]', ' ', text)
-    # Replace multiple spaces with single space
-    text = re.sub(r'\s+', ' ', text)
-    # remove # from markdown
-    text = re.sub(r'#+', '', text)
-    text = re.sub(r'<[^>]*>', '', text)
-    # remove leading and trailing non-alphabet characters
-    text = re.sub(r'^[^a-zA-Z]+|[^a-zA-Z]+$', '', text)
-
-    # remove from user query
-    text = re.sub(r'supergpt', '', text)
-    text = re.sub(r'user', '', text)
-    
-    text = text.strip().split(" ")
-    text = [t for t in text if t != "-" and t != ""]
-    # words stemming
-    text = [ps.stem(t) for t in text]
-    return list(set(text))
+with open(f"{DB_PATH_DS}/nodes.pickle", 'rb') as f:
+    global nodes_ds
+    nodes_ds = pickle.load(f)
 
 
-bm25_retriever = BM25Retriever.from_defaults(nodes=nodes, tokenizer=clean_text, similarity_top_k=4)
-
+bm25_retriever = BM25Retriever.from_defaults(nodes=nodes, tokenizer=clean_text, similarity_top_k=5)
+bm25_retriever_ds = BM25Retriever.from_defaults(nodes=nodes_ds, tokenizer=clean_text, similarity_top_k=10)
 
 def get_query(input_text, start_tag, end_tag):
     # Construct the regular expression pattern to match text between start_tag and end_tag
     pattern = re.compile(rf'{re.escape(start_tag)}(.*?){re.escape(end_tag)}', re.DOTALL)
-
     # Use re.findall() to find all occurrences of the pattern in the input text
     matches = re.findall(pattern, input_text)
-
     return matches
 
 
@@ -153,17 +124,18 @@ def query_engine_router(index, type_):
     # vector_retriever = index.as_retriever(similarity_top_k=7)
     # hybrid_retriever = HybridRetriever(vector_retriever, bm25_retriever4) # 10 docs
     #step_decompose_transform = StepDecomposeQueryTransform(llm=LLM, verbose=True)
+    #rerank = SentenceTransformerRerank(model="cross-encoder/ms-marco-MiniLM-L-2-v2", top_n=4)
     #reranker = SentenceTransformerRerank(top_n=4, model="BAAI/bge-reranker-base")
     rankergpt = RankGPTRerank(
             llm=LLM,
-            top_n=4,
+            top_n=10,
             verbose=True,
         )
 
-    query_engine, my_retriever = None, None
+    query_engine = None
     if type_ == "hybrid": # hybrid
         vector_retriever = index.as_retriever(similarity_top_k=7)
-        hybrid_retriever = HybridRetriever(vector_retriever, bm25_retriever) # 10 docs
+        hybrid_retriever = HybridRetriever(vector_retriever, bm25_retriever) # 15 docs
 
         query_engine = RetrieverQueryEngine.from_args(
             retriever=hybrid_retriever,
@@ -175,7 +147,7 @@ def query_engine_router(index, type_):
         )
     elif type_ == "bm25_spec": # bm25
         query_engine = RetrieverQueryEngine.from_args(
-            retriever=bm25_retriever,
+            retriever=bm25_retriever_ds,
             llm=LLM,
             streaming=True,
             text_qa_template=SYS_PROMPT,
@@ -184,23 +156,8 @@ def query_engine_router(index, type_):
     return query_engine
 
 
-@cl.cache
-def load_context():
-    Settings.llm = LLM
-    Settings.embed_model = EMBEDDING
-    Settings.num_output = 512
-    Settings.context_window = 8192
-
-    index = VectorStoreIndex.from_vector_store(
-        vector_store=VECTOR_STORE,
-        embed_model=EMBEDDING,
-    )
-    return index
-
-
 @cl.on_chat_start
 async def start():
-    #index = load_context()
     cl.user_session.set("counter", 0)
     cl.user_session.set("query_engine_bm25_s", query_engine_router(index, "bm25_spec"))
     cl.user_session.set("query_engine_hybrid", query_engine_router(index, "hybrid"))
@@ -219,6 +176,8 @@ async def set_sources(response, response_message):
 
     above_zero_nodes = []
     for sr in response.source_nodes:
+        # print(sr.node.text)
+        print(sr.get_score())
         if sr.get_score() > 0.03:
             above_zero_nodes.append(sr)
 
@@ -247,15 +206,16 @@ async def set_sources(response, response_message):
         elements.append(cl.Pdf(name=pdf, display="page", path=f"./SOURCE_DOCUMENTS/{pdf}"))
         count += 1
     response_message.elements = elements
-    response_message.content += "\n\nSources: " + ", ".join(label_list)
-    response_message.content += "\nSource pdfs: " + ", ".join(pdfs)
+
+    if len(above_zero_nodes) > 0:
+        response_message.content += "\n\nSources: " + ", ".join(label_list)
+        response_message.content += "\nSource pdfs: " + ", ".join(pdfs)
     await response_message.update()
 
 
 @cl.on_message
 async def main(user_message: cl.Message):
-    index = load_context()
-    n_history_messages = 8
+    n_history_messages = 15
     counter = cl.user_session.get("counter")
 
     message_history = cl.user_session.get("message_history")
@@ -279,17 +239,18 @@ async def main(user_message: cl.Message):
     user_query = get_query(prompt_template, '<|end_header_id|>', '<|eot_id|>')
     user_query = user_query[counter][2:]
     user_query = user_query.lower()
-    product_prefix = ['ars-', 'as-', 'asg-', 'ssg-', 'sys-', '4u', '1u', '2u', '5u', '8u', '10u']
+    print(user_query)
+    query_words = user_query.split(' ')
+    product_prefix = ['ars-', 'as-', 'asg-', 'ssg-', 'sys-']
+    skus = ['4u', '1u', '2u', '5u', '8u', '10u']
 
     query_engine = None
-    if any(prefix in user_query for prefix in product_prefix):
+    if any(prefix in user_query for prefix in product_prefix) or any(sku in query_words for sku in skus):
         print("bm25s")
         query_engine = cl.user_session.get("query_engine_bm25_s") # 4 docs
     else:
         print("hybrid")
         query_engine = cl.user_session.get("query_engine_hybrid") # 10 docs
-
-
 
     response = await cl.make_async(query_engine.query)(prompt_template)
     
@@ -301,12 +262,12 @@ async def main(user_message: cl.Message):
     await assistant_message.send()
 
     message_history.append({"author": "user", "content": user_message.content})
-    #message_history.append({"author": "SuperGPT", "content": assistant_message.content})
+    message_history.append({"author": "SuperGPT", "content": assistant_message.content})
     message_history = message_history[-n_history_messages:]
     cl.user_session.set("message_history", message_history)
 
     if response.source_nodes:
         await set_sources(response, assistant_message)
 
-    counter = counter+1 if counter != 8 else 0
+    counter = counter+1 if counter != 15 else 0
     cl.user_session.set("counter", counter)
